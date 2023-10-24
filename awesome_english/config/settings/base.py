149ad267c9767511c0be50058c90975f3cc9b0ad
kk-gc <kk.gc.lp@gmail.com>
2023-10-24 22:13:37 +0100

    Change db engine from postgres to sqlite3

--- awesome_english/config/settings/base.py
+++ awesome_english/config/settings/base.py
@@ -47,11 +47,17 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    "default": env.db(
-        "DATABASE_URL",
-        default="postgres:///awesome_english",
-    ),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+# DATABASES = {
+#     "default": env.db(
+#         "DATABASE_URL",
+#         default="postgres:///awesome_english",
+#     ),
+# }
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
