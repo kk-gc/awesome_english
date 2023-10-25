eb713291ceb16be564b165c938359efa35ac02f4
kk-gc <kk.gc.lp@gmail.com>
2023-10-25 09:10:42 +0100

    Add {user_name}.pythonanywhere.com to ALLOWED_HOSTS

--- awesome_english/config/settings/local.py
+++ awesome_english/config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="doXajXFR7ppWQLjpQLjvsWrrZ9pZGGOMF7B6PAVMExCUZpN9S6K98IXbILC8aClh",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "kkgc.pythonanywhere.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
